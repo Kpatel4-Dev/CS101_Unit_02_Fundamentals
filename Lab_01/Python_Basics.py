168aa5530d09c5a69095d4091349f91cdaefb042
Kavita Patel <kpatel@miracosta.edu>
2025-08-03 19:17:19 -0700

    Create Python_Basics.py

---
+++ Lab_01/Python_Basics.py
@@ -0,0 +1,58 @@
+'''
+
+Lab 01 - Programming in Python: Data, Functions, and Console I/O
+
+Description: A program to calculate annual car emissions.
+
+Developer Name(s): 
+
+Date: 
+
+'''
+##########################################
+# IMPORTS:
+#   modules needed for program
+##########################################
+
+
+##########################################
+# FUNCTIONS:
+#   functions useful to main program
+##########################################
+
+### Step 2 Create your function definition for calc_car_emissions here
+
+'''Calculates the average annual vehicle emissions using equation from cell C9
+  Parameters
+  ----------
+  mpg
+    vehicle's average miles per gallon
+  avg_miles_driven
+    average number of miles driven per year
+'''
+
+
+
+##########################################
+# MAIN PROGRAM:
+#   beginning of running program
+##########################################
+
+# Get input from user
+# Since we are going to be calling the function calc_car_emissions(mpg, avg_miles_driven)
+# We know that we are going to need a variable for mpg and avg_miles_driven
+
+
+
+
+# We now have the two variables we need for our calc_car_emissions, so we can call it with those values
+# We are also going to assign the value to a variable so we can use it later
+
+
+# Print out the values
+
+
+# Calculate and print the US average
+
+
+# Calculate and print the two comparison values
